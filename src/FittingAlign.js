ef19731b9c1ccdfffe71dc3ea18bae7f72c63246
Shachi16 <shachisolanki1108@gmail.com>
2019-12-06 12:12:06 -0600

    Added functionality to compute and display matrix for global, local, and fitting alignments

---
+++ src/FittingAlign.js
@@ -0,0 +1,66 @@
+const UP = (-1,0)
+const LEFT = (0, -1)
+const TOPLEFT = (-1, -1)
+const ORIGIN = (0, 0)
+
+export function fittingAlign(v, w, match, mismatch, gap) {
+  //Initialize variables
+  var M = []
+  var pointers = []
+  for (var i = 0; i < v.length; i++) {
+    M.push([])
+    pointers.push([])
+    for (var j = 0; j < w.length; j++) {
+      M[i].push(0);
+      pointers[i].push(ORIGIN);
+    }
+  }
+
+  //Construct Matrix M
+  for (var i = 0; i < v.length; i++) {
+    for (var j = 0; j < w.length; j++) {
+      var max_val = Number.NEGATIVE_INFINITY;
+      var dir = ORIGIN;
+      if (i == 0) {
+        max_val = 0;
+        dir = ORIGIN;
+      }
+
+      if (i > 0 && j > 0 && v[i] === w[j]) {
+        var temp = M[i - 1][j - 1] + match;
+        if (temp > max_val) {
+          max_val = temp;
+          dir = TOPLEFT;
+        }
+      }
+
+      if (i > 0 && j > 0 && v[i] != w[j]) {
+        var temp = M[i - 1][j - 1] + mismatch;
+        if (temp > max_val) {
+          max_val = temp;
+          dir = TOPLEFT;
+        }
+      }
+
+      if (j > 0) {
+        var temp = M[i][j - 1] + gap
+        if (temp > max_val) {
+          max_val = temp;
+          dir = LEFT;
+        }
+      }
+
+      if (i > 0) {
+        var temp = M[i - 1][j] + gap
+        if (temp > max_val) {
+          max_val = temp;
+          dir = UP;
+        }
+      }
+      M[i][j] = max_val;
+      pointers[i][j] = dir;
+    }
+  }
+
+  return { M, pointers }
+}
---
+++ src/Home.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import Matrix from './Matrix';
+import { Button } from 'semantic-ui-react';
+import { globalAlign } from './GlobalAlign.js';
+import { fittingAlign } from './FittingAlign.js';
+import { localAlign } from './LocalAlign.js';
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          matrix: [[]],
+          S1: '',
+          S2: '',
+          match: 0,
+          mismatch: 0,
+          gap: 0,
+          showMatrix: false,
+          algorithm: 'global'
+        }
+    }
+
+    handleClick = () => {
+      const { algorithm } = this.state;
+      var s1 = '-' + document.getElementById('input_s1').value;
+      var s2 = '-' + document.getElementById('input_s2').value;
+      var match = parseInt(document.getElementById('input_match').value);
+      var mismatch = parseInt(document.getElementById('input_mismatch').value);
+      var gap = parseInt(document.getElementById('input_gap').value);
+      var result;
+      if (algorithm === 'global') {
+        result = globalAlign(s1, s2, match, mismatch, gap);
+      }
+      else if (algorithm === 'fitting') {
+        result = fittingAlign(s1, s2, match, mismatch, gap);
+      }
+      else {
+        result = localAlign(s1, s2, match, mismatch, gap);
+      }
+      const { M } = result;
+      this.setState({matrix: M, showMatrix: true, S1: s1, S2: s2})
+    }
+
+    handleChange = () => {
+      if (document.getElementById('btn_fitting').checked) {
+          this.setState({algorithm: 'fitting'});
+      }
+      else if (document.getElementById('btn_local').checked) {
+          this.setState({algorithm: 'local'});
+      }
+      else {
+        this.setState({algorithm: 'global'});
+      }
+    }
+
+    render() {
+        const { matrix, S1, S2, showMatrix } = this.state;
+        return (
+          <div>
+            <h1> Sequence Aligner </h1>
+            <form>
+              <fieldset>
+                <legend>Select Alignment Algorithm</legend>
+                <p>
+                  <input onChange={this.handleChange} type="radio" name="algorithm" id="btn_global" value="global" defaultChecked/>
+                  <label for="btn_global" class="algorithm_select">Global</label>
+                </p>
+                <p>
+                  <input onChange={this.handleChange} type="radio" name="algorithm" id="btn_fitting" value="fitting"/>
+                  <label for="btn_fitting" class="algorithm_select">Fitting</label>
+                </p>
+                <p>
+                  <input onChange={this.handleChange} type="radio" name="algorithm" id="btn_local" value="local"/>
+                  <label for="btn_local">Local</label>
+                </p>
+              </fieldset>
+              <fieldset>
+                <legend>Enter Sequences</legend>
+                <p>
+                  <label for="input_s1">Sequence 1</label>
+                  <input type="text" id="input_s1"/>
+                </p>
+                <p>
+                  <label for="input_s2">Sequence 2</label>
+                  <input type="text" id="input_s2"/>
+                </p>
+              </fieldset>
+              <fieldset>
+                <legend>Enter Scoring Function</legend>
+                  <label for="input_match">Match</label>
+                  <input class="score_input" type="number" id="input_match"/>
+                  <label for="input_mismatch">Mismatch</label>
+                  <input class="score_input" type="number" id="input_mismatch"/>
+                  <label for="input_gap">Gap</label>
+                  <input class="score_input" type="number" id="input_gap"/>
+              </fieldset>
+            </form>
+            <div class="display">
+              <Button id="btn_compute" onClick={this.handleClick}>Compute Alignment</Button>
+              { showMatrix &&
+                <Matrix matrix={matrix} longS={S1.length >= S2.length ? S1 : S2} shortS={S1.length < S2.length ? S1 : S2}/>
+              }
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Home;
